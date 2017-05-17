52ce6d323632b9677c18ae871bbd9475a78ef95f
Rodrigo Castro <rfocastro@sp.r7.com>
2017-05-17 12:00:26 -0300

    finalizando configuracoes de email

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -8,7 +8,15 @@ class Backoffice::SendMailController < ApplicationController
   end
 
   def create
-    AdminMailer.send_message(current_admin, params[:'recipient-text'], params[:'subject-tex'], params[:'message-tex']).deliver_now
+    begin
+      AdminMailer.send_message(current_admin, params[:'recipient-text'], params[:'subject-tex'], params[:'message-tex']).deliver_now
+      @notfiy_message = 'Email enviado com sucesso'
+      @notfiy_flag = 'success'
+    rescue
+      @notfiy_message = 'Erro ao enviar email. Tent novamente'
+      @notfiy_flag = 'error'
+    end
+
     respond_to do |format|
       format.js
     end
