8a6b1f8ab753506bb963acc1c35fac48aa0dfdc9
Rodrigo Castro <rfocastro@sp.r7.com>
2017-05-22 11:25:16 -0300

    ignorando images e modificando tamanho

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -6,7 +6,7 @@ class Ad < ActiveRecord::Base
   scope :last_six, -> { limit(6).order(created_at: :desc) }
 
   # paperclip
-  has_attached_file :picture, styles: { medium: "320x150>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
 
