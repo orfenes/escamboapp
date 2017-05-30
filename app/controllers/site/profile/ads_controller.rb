538318c5ada963a79a78c360b2e27ea6cebd42c0
Rodrigo Castro <rfocastro@sp.r7.com>
2017-05-30 14:32:52 -0300

    upload de imagens

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,6 +1,7 @@
 class Site::Profile::AdsController < Site::ProfileController
 
-  before_action :set_ad, only: [:edit]
+  before_action :set_ad, only: [:edit, :update]
+  # before_action :params_ad, only: [:update]
 
   def index
     @ads = Ad.to_then(current_member.id)
@@ -9,10 +10,23 @@ class Site::Profile::AdsController < Site::ProfileController
   def edit
   end
 
+  def update
+    if @ad.update(params_ad)
+      redirect_to site_profile_ads_path,
+      notice: "Anuncio atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_ad
     @ad = Ad.find(params[:id])
+  end
+
+  def params_ad
+    params.require(:ad).permit(:id, :title, :category_id, :price, :description, :picture)
   end
 
 end
