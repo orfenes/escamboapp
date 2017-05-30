db30fc2a0892376ab813ede944e227ae95f79cff
Rodrigo Castro <rfocastro@sp.r7.com>
2017-05-30 14:20:08 -0300

    layout sucesso

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,13 +1,18 @@
 class Site::Profile::AdsController < Site::ProfileController
+
+  before_action :set_ad, only: [:edit]
+
   def index
     @ads = Ad.to_then(current_member.id)
   end
 
   def edit
-    #
   end
 
-  def update
-    #
+  private
+
+  def set_ad
+    @ad = Ad.find(params[:id])
   end
+
 end
