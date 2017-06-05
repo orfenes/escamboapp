1621271acc072ffd1122e8496166f08f4a0af3e8
Rodrigo Castro <rfocastro@sp.r7.com>
2017-06-05 11:56:53 -0300

    configurando datepicker

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,8 +10,10 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( site.js
                                                   site/profile.js
+                                                  site/profile/ads.js
                                                   backoffice.js )
 
 Rails.application.config.assets.precompile += %w( site/profile.css
+                                                  site/profile/ads.css
                                                   site.css
                                                   backoffice.css )
