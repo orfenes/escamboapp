4d5102c0ea2b7a0dd050e10d839afb7a1dd3150c
Rodrigo Castro <rfocastro@sp.r7.com>
2017-06-20 17:26:40 -0300

    correcao nas rotas devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
 
+  before_action :store_current_location, :unless => :devise_controller?
+
+  # Oundit
   include Pundit
 
   # manages Pundit Erros
@@ -14,19 +17,25 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def layout_by_resource
-    if devise_controller? && resource_name == :admin
-      "backoffice_devise"
-    elsif devise_controller? && resource_name == :member
-      "site_devise"
-    else
-      "application"
+    def layout_by_resource
+      if devise_controller? && resource_name == :admin
+        "backoffice_devise"
+      elsif devise_controller? && resource_name == :member
+        "site_devise"
+      else
+        "application"
+      end
     end
-  end
 
   def user_not_authorized
     flash[:alert] = "Você não tem permissao para executar essa ação"
     redirect_to(request.referrer || root_path)
   end
+
+  private
+
+    def store_current_location
+      store_location_for(:member, request.url)
+    end
 
 end
